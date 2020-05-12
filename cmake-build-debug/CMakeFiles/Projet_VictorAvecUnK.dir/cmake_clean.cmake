aa7965d44d26d7f78d3c5e038a7474c1d5767be2
bray <bray@etud.insa-toulouse.fr>
2020-05-12 09:37:53 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/Projet_VictorAvecUnK.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projet_VictorAvecUnK.dir/main.cpp.obj"
+  "Projet_VictorAvecUnK.exe"
+  "Projet_VictorAvecUnK.exe.manifest"
+  "Projet_VictorAvecUnK.pdb"
+  "libProjet_VictorAvecUnK.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Projet_VictorAvecUnK.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
